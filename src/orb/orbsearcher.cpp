59ee1461c3e48c7c2bb0bb4bcdf2a5615b6d34dc
linzuk <linzuk666@163.com>
2017-07-12 10:20:05 +0800

    80

--- src/orb/orbsearcher.cpp
+++ src/orb/orbsearcher.cpp
@@ -286,7 +286,7 @@ u_int32_t ORBSearcher::processSimilar(ORBIndex *index, SearchRequest &request,
     cout << "time: " << getTimeDiff(t[5], t[6]) << " ms." << endl;
     cout << "Returning the results. " << endl;
 
-    returnResults(index, rerankedResults, request, 30);
+    returnResults(index, rerankedResults, request, 80);
 
     return SEARCH_RESULTS;
 }
